312e28b33cbf57dd3b1e2794af4d73e0890c1e7a
Nic009 <nicojens.jaeger@gmail.com>
2022-02-11 18:58:35 +0100

    Finished Aufgabe_5.py and Aufgabe_6.py

---
+++ Aufgabe_5.py
@@ -0,0 +1,12 @@
+# Übungsaufgaben zu Schleifen
+# Aufgabe 5
+# @Nico Jäger
+
+
+inpInteger = input("Bitte geben sie ihre Ganzzahligen Positiven Zahlen ein (Seperator = lehrzeichen): ")
+integer = list(map(int, inpInteger.split()))
+integer.append(0)
+integer.pop()
+
+mean = sum(integer) / len(integer)
+print("Ihr Durchschnitt ist:", mean)
---
+++ Aufgabe_6.py
@@ -0,0 +1,8 @@
+# Übungsaufgaben zu Schleifen
+# Aufgabe 6
+# @Nico Jäger
+
+
+for i in range (0, 3000, 1):
+    if (i % 4 == 0) and (i % 100 != 0) or (i % 400 == 0):
+        print(i, "ist ein Schaltjahr!")
