c4661755b8c62ca66320b5ee64439ad21b6ee0a9
MESS1OS <nicojens.jaeger@gmail.com>
2022-03-23 14:30:11 +0100

    small changes

--- Aufgabe_2.py
+++ Aufgabe_2.py
@@ -4,4 +4,4 @@
 
 for temperatureF in range(0, 301, 5):
     temperatureC = (temperatureF - 32) * (5/9)
-    print(temperatureF, "  ", temperatureC.__round__(4))
+    print(temperatureF, "  ", temperatureC)
--- Aufgabe_3a-b-c-d-e.py
+++ Aufgabe_3a-b-c-d-e.py
@@ -75,9 +75,12 @@ m = float(input("Bitte geben sie ihren gewünschte Steigung ein: "))
 b = float(input("Bitte geben sie den gewünschten Y-Achsen Abstand ein: "))
 i = float(input("Bitte geben sie die gewünschte Schrittweite ein: "))
 print("Alle Positiven Werte: ")
+
 for x in np.arange(-5.3, 3.6, i):
     y = m * x + b
     if y > 0:
         count += 1
-        print("f(" + str(x.__round__(1)) + ") =", y.__round__(2))
+        print("f(" + str(x.__round__(2)) + ") =", y.__round__(2))
 print("Es gibt", count, "positive Funktionswerte")
+
+
